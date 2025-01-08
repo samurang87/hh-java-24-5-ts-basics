2fc0a42fa402594062eff3d12094b2e010aac911
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2025-01-08 11:10:49 +0100

    feat: Assign grades to subjects

--- src/03_exercise_ts_types.ts
+++ src/03_exercise_ts_types.ts
@@ -1,7 +1,10 @@
 // Step 1: Write a function that outputs a student with their grades, where a student
 // should be output with first name, last name, age, and a list of grades.
 
-type Grade = "1" | "2" | "3" | "4" | "5" | "6" | "A" | "B" | "C" | "D" | "E" | "F" | undefined;
+type Grade = {
+    subject: string;
+    value: "1" | "2" | "3" | "4" | "5" | "6" | "A" | "B" | "C" | "D" | "E" | "F" | undefined;
+}
 
 type Student = {
     firstName: string,
@@ -10,11 +13,18 @@ type Student = {
     grades: Grade[]
 }
 
+function stringGrade(grade: Grade): string {
+    return `${grade.subject}: ${grade.value === undefined ? "*" : grade.value}`;
+}
+
 function printStudent(student: Student): void {
     // console.log(`Student: ${student.firstName} ${student.lastName}, Age: ${student.age}, Grades: ${student.grades}`);
-    console.log(`${student.firstName} ${student.lastName} (${student.age})`);
-    console.log(`=`.repeat(30));
-    console.log(`Grades: ${student.grades.map(grade => grade === undefined ? "*": grade).join(",")}`);
+    const headline = `${student.firstName} ${student.lastName} (${student.age})`;
+    console.log(headline);
+    console.log(`=`.repeat(headline.length));
+    for (const grade of student.grades) {
+        console.log(stringGrade(grade));
+    }
     console.log(``);
 }
 
@@ -26,21 +36,57 @@ const john: Student = {
     firstName: "John",
     lastName: "Green",
     age: 25,
-    grades: ["1", "2", "3"]
+    grades: [
+        {
+            subject: "Math",
+            value: "A",
+        },
+        {
+            subject: "English",
+            value: "B",
+        },
+        {
+            subject: "Science",
+            value: "A",
+        }]
 }
 
 const mary: Student = {
     firstName: "Mary",
     lastName: "Red",
     age: 22,
-    grades: [undefined, "B", "C"]
+    grades: [
+        {
+            subject: "Math",
+            value: "1",
+        },
+        {
+            subject: "English",
+            value: undefined,
+        },
+        {
+            subject: "Science",
+            value: "A",
+        }]
 }
 
 const james: Student = {
     firstName: "James",
     lastName: "Blue",
     age: 20,
-    grades: ["3", "1", undefined]
+    grades: [
+        {
+            subject: "Math",
+            value: "2",
+        },
+        {
+            subject: "English",
+            value: "1",
+        },
+        {
+            subject: "Science",
+            value: undefined,
+        }]
 };
 
 printAllStudents([john, mary, james]);
